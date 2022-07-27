ff2f0ade6be2e8d298af2fcb3e66011f90a22204
Rajat Garg <rajat.garg@BLRETV-Y40Y60XC.local>
2022-07-27 14:26:16 +0530

    Added user bean

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/bean/User.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/bean/User.java
@@ -8,5 +8,58 @@ package com.flipkart.bean;
  *
  */
 public class User {
-
+	
+	private String username;
+	private int userID;
+	private String email;
+	private String password;
+	
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	/**
+	 * @return the userID
+	 */
+	public int getUserID() {
+		return userID;
+	}
+	/**
+	 * @param userID the userID to set
+	 */
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
